12323ab0e0f6add4aac50a4470d83711b3426a6b
Bjørnar <bwendelborgrisdalen@gmail.com>
2019-12-06 21:23:35 +0100

    Day 4 completed

--- brisdalen/src/day4/Day4a.java
+++ brisdalen/src/day4/Day4a.java
@@ -2,6 +2,7 @@ package day4;
 
 public class Day4a {
 
+    private final String ORIGINAL_INPUT = "356261-846303";
     private String range = "356261-846303";
 
     private int low, high;
@@ -24,7 +25,7 @@ public class Day4a {
         int count = 0;
 
         for(int i = low; i <= high; i++) {
-            if(noDecreasingNumbers(i) && containsPair(i)) {
+            if(noDecreasingNumbers(i) && containsValidPair(i)) {
                 count++;
             }
         }
@@ -57,6 +58,65 @@ public class Day4a {
         }
 
         return containsPair;
+    }
+
+    private boolean containsValidPair(int number) {
+        int validPairs = 0;
+        int[] temp = integerToArray(number);
+
+        for(int i = 0; i < temp.length-1; i++) {
+            // Sjekk de 4 første sifferene
+            if(i < 4) {
+                // Se etter et par
+                if (temp[i] == temp[i + 1]) {
+                    // Se om paret ikke er en del av en større gruppe like tall (eks. ikke 444)
+                    if(i == 0) {
+                        if (temp[i + 1] != temp[i + 2]) {
+                            validPairs++;
+                            // Ikke vits å sjekke neste da i og i+1 allerede er et par
+                            i++;
+                        }else {
+                            // Om paret er en del av en større gruppe tall flyttes pekeren (i) 2 til høyre
+                            // hvis pekeren er på første eller andre siffer
+                            if(i <= 1) {
+                                i += 2;
+                                // Hvis pekeren er på 3. siffer (2) og er en del av flere like siffer
+                                // er det ingen vits å sjekke resten av tallene
+                            } else if(i == 2 || i == 3) {
+                                break;
+                            }
+                        }
+                    } else {
+                        if(temp[i+1] != temp[i+2] && temp[i] != temp[i-1]) {
+                            validPairs++;
+                            // Ikke vits å sjekke neste da i og i+1 allerede er et par
+                            i++;
+                        } else {
+                            // Om paret er en del av en større gruppe tall flyttes pekeren (i) 2 til høyre
+                            // hvis pekeren er på første eller andre siffer
+                            if(i <= 1) {
+                                i += 2;
+                                // Hvis pekeren er på 3. siffer (2) og er en del av flere like siffer
+                                // er det ingen vits å sjekke resten av tallene
+                            } else if(i == 2 || i == 3) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            // Sjekk de 2 siste sifferene
+            } else if(i == 4) {
+                // Sjekk om de 2 siste er et par, men ikke like det 3. siste sifferet
+                if(temp[i] == temp[i + 1] && temp[i] != temp[i-1]) {
+                    validPairs++;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return validPairs > 0;
     }
 
     private int[] integerToArray(int number) {
